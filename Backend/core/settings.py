e419ca0c39c2f7841360c3af0d5d3dc7692208c7
abdelhak-k <abdelhak.kadouci@ensia.edu.dz>
2026-05-02 20:27:56 +0100

    fix

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".leapcell.dev"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".leapcell.dev", ".leapcell.app"]
 
 
 # Application definition
