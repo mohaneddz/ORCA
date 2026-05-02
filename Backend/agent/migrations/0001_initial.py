fc8bba859c8ec7ff2b9006e983fc05d78b39412c
abdelhak-k <abdelhak.kadouci@ensia.edu.dz>
2026-05-02 20:39:17 +0100

    fix broken migration dependencies

--- Backend/agent/migrations/0001_initial.py
+++ Backend/agent/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ("organizations", "0007_organization_is_staff_organization_is_superuser"),
+        ("organizations", "0001_initial"),
     ]
 
     operations = [
--- Backend/gamification/migrations/0003_add_employee_and_employeeauthtoken.py
+++ Backend/gamification/migrations/0003_add_employee_and_employeeauthtoken.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ("gamification", "0002_add_employee_and_employeeauthtoken"),
-        ("organizations", "0008_add_employee_and_employeeauthtoken"),
+        ("organizations", "0001_initial"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
